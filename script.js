3c74cdbbfbc6f4844f2e95885e27ffc5f946fe79
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2023-07-07 00:52:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//typing animation  
+var typed = new Typed(".typing",{
+    String:["","Web Designer", "web Developer", "Graphic Designer", "YouTuber"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
